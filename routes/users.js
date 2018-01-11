e646c0a4fee2f2ff7f348359cf40af5fc06a8067
adplt <atriadplt@gmail.com>
2018-01-11 14:14:42 +0700

    change user API to post

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.post('/', function(req, res, next) {
+  return res.status(200).
+    json({
+      responseCode: '00',
+      responseMessage: 'Success',
+    });
 });
 
 module.exports = router;
